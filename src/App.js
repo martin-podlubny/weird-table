ea7333121be34b1887e9812e2ce8431ffdc311b2
martin-podlubny <martin.podlubny@gmail.com>
2022-06-20 17:12:36 +0100

    add the weird table (only renders last data point)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Table } from "@redgate/honeycomb-components";
+
+const WeirdTable = () => {
+  const tableColumns = [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Age",
+      accessor: "age",
+    },
+  ];
+
+  const tableData = [
+    {
+      name: "Tom",
+      age: "20",
+    },
+    {
+      name: "Dick",
+      age: "30",
+    },
+    {
+      name: "Harry",
+      age: "40",
+    },
+  ];
+
+  return <Table columns={tableColumns} data={tableData} />;
+};
+
 function App() {
-  return (
-    "hello"
-  );
+  return <WeirdTable />;
 }
 
 export default App;
